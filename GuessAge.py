8dd4984928729242c33ca5d680987fe13d53652d
Teo <teoramosruano@gmail.com>
2025-04-23 14:07:22 +0200

    Commit new computer

--- GuessAge.py
+++ GuessAge.py
@@ -2,4 +2,10 @@
 #Año de nacimiento
 #Ahora con un cálculo 
 #Calcula la edad a partir del año de nacimiento :)
-nombre = input("Please tellme your name")
+from datetime import datetime
+
+nombre = input("Please tellme your name ")
+anoDeNacer = int(input("When were you born? "))
+esteAno = datetime.now().year
+edad = esteAno - anoDeNacer
+print(f"Helo {nombre} you are {edad} years old")
