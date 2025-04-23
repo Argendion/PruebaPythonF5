7472e1b888c72324a26d36d0c44bde2a3454bd8a
Teo <teoramosruano@gmail.com>
2025-04-23 14:15:58 +0200

    Commit new computer

--- GuessAge.py
+++ GuessAge.py
@@ -9,3 +9,11 @@ anoDeNacer = int(input("When were you born? "))
 esteAno = datetime.now().year
 edad = esteAno - anoDeNacer
 print(f"Helo {nombre} you are {edad} years old")
+if(edad<17):
+    print("Get outta here green horn")
+elif(edad<22):
+    print("This dammed millenials")
+elif(edad<45):
+    print("At deaths door you are")
+else:
+    print("Careful the fossil is walking")
